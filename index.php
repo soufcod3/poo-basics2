ac11414cd3dcab0cd7c813cc5fa501addf0b2d34
soufcod3 <soufwra@gmail.com>
2021-10-17 22:10:24 +0200

    Comments added

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once 'Bicycle.php';
-require_once 'Car.php';
+// require_once 'Bicycle.php';
+// require_once 'Car.php';
 require_once 'Truck.php';
 
 $bicycle = new Bicycle('blue', 1);
-var_dump($bicycle);
+// var_dump($bicycle);
 
 $car = new Car('green', 4, 'electric');
-var_dump($car);
+// var_dump($car);
 
 $truck = new Truck('grey', 3, 'fuel', 10);
 $truck->setLoad(55);
